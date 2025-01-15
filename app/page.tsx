55d65b66312596e31654c9e1bc8dc18226777fe1
rishilahoti <rishilahoti99@gmail.com>
2025-01-15 15:50:31 +0530

    image enhancement

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,20 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="mt-14">
-			<div className="m-auto flex items-center justify-center rounded-2xl md:h-[80vh] md:w-[80%] md:justify-normal md:bg-[url(/background.svg)] md:bg-cover md:bg-center md:bg-no-repeat md:p-10">
+			<div className="m-auto flex items-center justify-center rounded-2xl md:h-[80vh] md:w-[80%] md:justify-normal md:p-10">
 				{/* drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] */}
+				<div className="md:absolute md:block hidden inset-x-20 inset-y-14 mt-20">
+					<Image
+						src="/background.png"
+						alt="background-img"
+                        fill={true}
+                        blurDataURL="/background.png"
+                        priority
+                        placeholder="blur"
+                        quality={100}
+						className="h-full w-full rounded-2xl object-center object-cover"
+					/>
+				</div>
 				<div className="flex h-[90%] w-[90%] flex-col justify-center gap-y-[30px] rounded-2xl bg-transparent bg-opacity-15 p-3 shadow-md backdrop-blur md:w-[55%] md:p-5">
 					<h3 className="mx-auto bg-gradient-to-t from-lime-200 to-lime-400 bg-clip-text text-[22px] font-extrabold text-transparent drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:mx-0 md:text-4xl">
 						ORGANIC / CONVENTIONAL
@@ -44,11 +56,13 @@ export default function Home() {
 
 const ButtonHover = () => {
 	return (
-		<div className="flex cursor-pointer flex-wrap justify-evenly md:justify-start gap-x-5">
+		<div className="flex cursor-pointer flex-wrap justify-evenly gap-x-5 md:justify-start">
 			<DrawOutlineButton>
 				<CloudDownload />
 			</DrawOutlineButton>
-			<DrawOutlineButton><Grip/></DrawOutlineButton>
+			<DrawOutlineButton>
+				<Grip />
+			</DrawOutlineButton>
 		</div>
 	);
 };
