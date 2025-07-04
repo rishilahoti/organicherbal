079915e4c42d274eb1acf7f9257321e5dd3446b8
rishilahoti <rishilahoti99@gmail.com>
2025-07-04 23:59:29 +0530

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: 'https://yourdomain.com',
+  siteUrl: 'https://organicherbal.in',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
